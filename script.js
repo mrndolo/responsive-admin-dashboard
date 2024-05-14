7000285410c7f0c09213a40421ab18723e8a0067
mrndolo <callistomutunga@gmail.com>
2024-05-14 12:13:18 +0300

    Theme Change/Toggle Functionality (Light & Dark Mode with JavaScript)

--- script.js
+++ script.js
@@ -4,11 +4,26 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// Show Sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
 
+// Close Sidebar
 closeBtn.addEventListener('click', ()=>{
     sideMenu.style.display = 'none';
+})
+
+// Change Theme
+themeToggler.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme-variables')
+
+    // The Long Way
+    // themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    // themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+    // or simply
+    themeToggler.querySelector('span').classList.toggle('active');
 })
