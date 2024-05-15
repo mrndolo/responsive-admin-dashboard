50cb45887d4e0469dce5dc1b3fc44972c36e0341
mrndolo <callistomutunga@gmail.com>
2024-05-15 09:29:39 +0300

    filling Recent Orders table with JavaScript

--- script.js
+++ script.js
@@ -27,3 +27,17 @@ themeToggler.addEventListener('click', () =>{
     // or simply
     //themeToggler.querySelector('span').classList.toggle('active');
 })
+
+//Fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
